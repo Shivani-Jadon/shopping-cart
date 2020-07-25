a20bfad2d11bb8079abfe51c4997b374aa53ae09
Shivani Jadon <shivanijadon98@gmail.com>
2020-07-25 17:38:09 +0530

    add navbar to the cart

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.css';
 import Cart from './Cart';
+import Navbar from './Navbar';
+import NavBar from './Navbar';
 
 
 function App() {
   return (
     <div className="App">
-      <h1><img style={{width:40, marginBottom:-10}} src="https://image.flaticon.com/icons/svg/1170/1170576.svg" alt="Cart"/>  My Shopping Cart</h1>
+      <header>
+        <h1><img style={{width:40, marginBottom:-10}} src="https://image.flaticon.com/icons/svg/1170/1170576.svg" alt="Cart"/>  My Shopping Cart</h1>
+        <NavBar />
+      </header>      
       <Cart />
     </div>
   );
---
+++ src/Navbar.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const Navbar = (props) => {
+
+        return (
+            <div style={styles.cart_detail}>
+                <img style={styles.cartImage} src="https://image.flaticon.com/icons/svg/3208/3208738.svg"></img>
+                <span style={styles.cartCount}>5</span>
+            </div>
+        );   
+}
+
+const styles = {
+    cart_detail : {
+        display : 'flex',
+        marginTop : 20
+    },
+    cartImage : {
+        width : 40
+    },
+    cartCount : {
+        height : 20,
+        padding : 5,
+        borderRadius : '50%',
+        background : 'crimson',
+        color : 'aliceblue',
+        marginLeft : -5,
+        fontWeight : 'bold',
+        textShadow : 'none'
+    }
+}
+
+export default Navbar;
