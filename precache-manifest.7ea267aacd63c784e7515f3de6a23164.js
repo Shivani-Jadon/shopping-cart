86b69d4767452283a00c481e6de48a0979cf0d17
Shivani Jadon <shivanijadon98@gmail.com>
2020-07-31 21:16:11 +0530

    Updates

---
+++ precache-manifest.7ea267aacd63c784e7515f3de6a23164.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bae370e9ca71dd7491be5c1ce302fb06",
+    "url": "/Shivani-Jadon/Indecision-app/index.html"
+  },
+  {
+    "revision": "407ca9399a2249ae44f3",
+    "url": "/Shivani-Jadon/Indecision-app/static/css/main.7f28d7aa.chunk.css"
+  },
+  {
+    "revision": "fa5fa3368d7bf3005820",
+    "url": "/Shivani-Jadon/Indecision-app/static/js/2.c43617e0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Shivani-Jadon/Indecision-app/static/js/2.c43617e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "407ca9399a2249ae44f3",
+    "url": "/Shivani-Jadon/Indecision-app/static/js/main.caafb046.chunk.js"
+  },
+  {
+    "revision": "db9aca3ec9bdc2cdcacd",
+    "url": "/Shivani-Jadon/Indecision-app/static/js/runtime-main.b228a5ca.js"
+  }
+]);
